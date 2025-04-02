967056bd82df121c94e9d3891313dccac5e3130e
Axel Rojas <axel.rojasretana@ucr.ac.cr>
2025-04-02 17:05:25 -0600

    add method to change interface matrix, method to put ship, interface logic, method to put interface text

--- include/GameMapUI.hpp
+++ include/GameMapUI.hpp
@@ -7,6 +7,22 @@
 
 using namespace sf;
 
+#define GRID_SIZE 10
+#define CELL_SIZE 50
+
+enum cellState{
+    WATER = 0,
+    SHIP = 1,
+    HIT = 2,
+};
+
+struct Ship{
+    int size;
+    int row, col;
+    bool horizontal;
+};
+
+
 class GameMap {
 public:
     // default constructor
@@ -16,11 +32,46 @@ public:
     void handleEvent(RenderWindow& window, Event& event);
     void draw(RenderWindow& window);
     void blackScreen(RenderWindow& window);
-private:
+    void render(RenderWindow& window);
+    void drawGrid(RenderWindow& window);
+    void drawShips(RenderWindow& window);
+    void drawInfo(RenderWindow& window);
+    void placeShip(int x, int y, int size, bool horizontal);
+    void removeShip(int x, int y);
+    void attackShip(int x, int y);
+    void updateMatrix(int **matrix);
+    void printText(RenderWindow& window, const std::string& text, int x, int y);
+    
 
+private:
     // background
     Texture backgroundTexture;
     Sprite backgroundSprite;
+    Texture shipTexture;
+    Font font;
+    Text infoText;
+
+    // grid
+    int board[GRID_SIZE][GRID_SIZE]={0};
+
+    std::vector<Ship> ships;
+    int shipCount = 0;
+    int shipSize = 1;
+    bool attack = false;
+    bool horizontal = false;
+
+    std::map<int, int>shipLimits = {
+        {2, 1},
+        {3, 2},
+        {4, 1},
+        {5, 1}
+    };
+    std::map<int, int>shipCountMap = {
+        {2, 0},
+        {3, 0},
+        {4, 0},
+        {5, 0}
+    };
 };
 
 #endif // !GAMEMAPUI
--- src/GameMapUI.cpp
+++ src/GameMapUI.cpp
@@ -4,64 +4,163 @@
 #include <unistd.h>
 #include <thread>
 
-GameMap::GameMap(){
-    // Load background
-    if (!backgroundTexture.loadFromFile("assets/img/background.png")) {
-        std::cerr << "Error al cargar la textura del fondo." << std::endl;
-    }
-    backgroundSprite.setTexture(backgroundTexture);
-    // Center background
-    //backgroundSprite.setPosition(Vector2f(35, 0));
 
+
+GameMap::GameMap(){
+  if (!shipTexture.loadFromFile("assets/img/ship.png")) {
+    std::cerr << "Error loading ship texture" << std::endl;
+  }
+
+  if (!backgroundTexture.loadFromFile("assets/img/board.png")) {
+    std::cerr << "Error loading ship texture" << std::endl;
+  }
+
+  if (!font.loadFromFile("assets/font/Arcade.ttf")) {
+    std::cerr << "Error loading font" << std::endl;
+    // manejar error
+}
+  infoText.setFont(font);
+  backgroundSprite.setTexture(backgroundTexture);
+
+  //if (!hitTexture.loadFromFile("explosion.png")) return false;
+}
+
+void GameMap::printText(RenderWindow& window, const std::string& text, int x,
+      int y) {
+  infoText.setString(text);
+  infoText.setCharacterSize(24);
+  infoText.setFillColor(Color::Black);
+  infoText.setStyle(Text::Bold);
+  infoText.setPosition(x, y);
+  window.draw(infoText);
+}
+
+void GameMap::render(RenderWindow& window) {
+  // Draw background
+  window.clear(sf::Color::Blue);
+  draw(window);
+  drawShips(window);
+  printText(window, "0", 575, 10);
+  printText(window, "5", 575, 30);
+  window.display();
+}
+void GameMap::drawShips(RenderWindow& window) {
+  Sprite shipSprite(shipTexture);
+  shipSprite.setScale(
+      (float)CELL_SIZE / shipTexture.getSize().x,
+      (float)CELL_SIZE / shipTexture.getSize().y
+  );
+
+  for (int row = 0; row < GRID_SIZE; ++row) {
+      for (int col = 0; col < GRID_SIZE; ++col) {
+          if (board[row][col] == SHIP) {
+              shipSprite.setPosition(col * CELL_SIZE, row * CELL_SIZE);
+              window.draw(shipSprite);
+          }
+      }
+  }
+}
+
+void GameMap::updateMatrix(int **matrix) {
+  for (int row = 0; row < GRID_SIZE; ++row) {
+    for (int col = 0; col < GRID_SIZE; ++col) {
+        board[row][col] = matrix[row][col];
+    }
+  }
 }
 
 
+void GameMap::drawGrid(RenderWindow& window) {
+    // Draw grid
+  for (int i = 0; i < GRID_SIZE; i++) {
+    Vertex line[] = { Vertex(Vector2f(i*CELL_SIZE, 0), Color::White),
+        Vertex(Vector2f(i*CELL_SIZE, GRID_SIZE*CELL_SIZE), Color::White) };
+    window.draw(line, 2, Lines);
+    Vertex line2[] = { Vertex(Vector2f(0, i*CELL_SIZE), Color::White),
+        Vertex(Vector2f(GRID_SIZE*CELL_SIZE, i*CELL_SIZE), Color::White)};
+    window.draw(line2, 2, Lines);
+  }
+}
 
 void GameMap::run(RenderWindow& window) {
-    bool endRound = false;
-    
-    while (window.isOpen()) {
-        
-        endRound = false;
-
-        while (endRound == false) {
-            Event event;
-            while (window.pollEvent(event)) {
-                this->handleEvent(window, event);
-            }
-
-            
-
-            // Sprites draw
-            window.clear();
-            draw(window);
-            window.display();
-
-        // new game wait        
-        
-        }
+  bool endRound = false;
+  while (window.isOpen()) {
+    endRound = false;
+    while (endRound == false) {
+      Event event;
+      while (window.pollEvent(event)) {
+        this->handleEvent(window, event);
+      }
+      // Sprites draw
+      window.clear();
+      render(window);
+      window.display();
+      // new game wait     
     }
+  }
 }
 
-void GameMap::blackScreen(RenderWindow& window){
-        window.clear(sf::Color::Black);
-        window.display();
-        sf::sleep(sf::seconds(3));
+void GameMap::attackShip(int row, int col) {
+  if (board[row][col] == SHIP) board[row][col] = HIT;
 }
 
+void GameMap::placeShip(int row, int col, int size, bool horizontal) {
+  for(int i = 0; i < size; i++) {
+    int r = row + (horizontal ? 0 : i);
+    int c = col + (horizontal ? i : 0);
+    board[r][c] = SHIP;
+  }
+}
+
+void GameMap::blackScreen(RenderWindow& window) {
+  sf::sleep(sf::seconds(3));
+  window.clear(sf::Color::Black);
+  window.display();
+  sf::sleep(sf::seconds(3));
+}
 
 void GameMap::handleEvent(RenderWindow& window, Event& event) {
-    if (event.type == Event::Closed) {
-        window.close();
+  if (event.type == Event::Closed) {
+    window.close();
+  }
+  if (event.type == Event::MouseButtonPressed) {
+    // Get mouse position
+    // Check if mouse is inside the grid
+    if (event.mouseButton.x >= 0 && event.mouseButton.x <= GRID_SIZE*CELL_SIZE &&
+        event.mouseButton.y >= 0 && event.mouseButton.y <= GRID_SIZE*CELL_SIZE) {
+      int col = event.mouseButton.x / CELL_SIZE;
+      int row = event.mouseButton.y / CELL_SIZE;
+      if (event.mouseButton.button == Mouse::Left) {
+        if (attack){
+          attackShip(row, col);
+        } else {
+          placeShip(row, col, shipSize, horizontal);
+        }
+      }
+      if (event.mouseButton.button == Mouse::Right) {
+        // Remove ship
+        int **matrix = new int*[GRID_SIZE];
+        for (int i = 0; i < GRID_SIZE; ++i) {
+          matrix[i] = new int[GRID_SIZE];
+          for (int j = 0; j < GRID_SIZE; ++j) {
+            matrix[i][j] = 0;
+          }
+        }
+        matrix[1][1] = SHIP;
+        updateMatrix(matrix);
+        removeShip(row, col);
+      }
     }
-    if (event.type == Event::KeyPressed) {
-        blackScreen(window);
-    }
+  }
 }
 
+void GameMap::removeShip(int row, int col) {
+  printf("Removing ship at (%d, %d)\n", row, col);
+  board[row][col] = WATER;
+}
 
 void GameMap::draw(RenderWindow& window) {
-    // Draw background
-    window.draw(backgroundSprite);
+  // Draw background
+  window.draw(backgroundSprite);
 }
 
