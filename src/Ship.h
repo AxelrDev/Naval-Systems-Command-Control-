27a39038fad429d0c63fe4e6ad03bb4a08ff7899
jsalasl80 <jorge.salaslau@ucr.ac.cr>
2025-04-02 18:46:29 -0600

    Implemented .h ship completely

--- src/Ship.h
+++ src/Ship.h
@@ -3,4 +3,32 @@
 
 #include "Includes.h"
 
+class Ship {
+protected:
+    std::string name;
+    int health;
+    int price;
+    std::vector<int> Properties; // <x,y,length> TENTATIVO Jose pablo resuelve como quiera llevarlo
+    std::set<int> elementSet; //Used to check if a number is present for erasing or searching, random selecting for erasure
+    std::vector<int> elements; //Used for algorithms
+
+public:
+    Ship(std::string name, int price);
+
+    virtual ~Ship() = default;
+    
+    virtual int search(int target) = 0;
+    virtual int insert (int element) = 0;
+    virtual int remove(int element) = 0;
+
+    virtual void upgrade();
+
+    int getPrice()const;
+    vector<int> getProperties();
+    void setProperties(int x, int y, int z); // NOT FULLY IMPLEMENTED
+    std::string getName() const;
+
+    virtual void logOperation(const std::string& type, int iterations, double time); // NOT FULLY IMPLEMENTED
+}
+
 #endif
